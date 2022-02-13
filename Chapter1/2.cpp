b402da244ab13f779e6fe13b4e4988e23d165cb0
keigo_shimada <e215730@ie.u-ryukyu.ac.jp>
2022-02-13 14:17:48 +0900

    :+1: `Add` 2.cpp

---
+++ Chapter1/2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using ui = unsigned int;
+
+ui gcd(ui const a, ui const b) {
+    return (b == 0) ? a : gcd(b, a%b);
+}
+
+ui gcd2(ui a, ui b) {
+    while(b != 0) {
+        ui r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+int main(void) {
+    cout << "Input numbers:";
+    ui a, b;
+    cin >> a >> b;
+    cout
+        << "rec gcd(" << a << ", " << b << ")="
+        << gcd(a, b) << endl;
+    cout
+        << "    gcd(" << a << ", " << b << ")="
+        << gcd2(a, b) << endl;
+}
